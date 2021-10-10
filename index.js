6033695c117438978d25a5be3330a6f62ffba184
Astr0 <92247349+astr0-tutorials@users.noreply.github.com>
2021-10-10 09:49:33 +0100

    Added index and packages

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Collection, Client, Discord } = require('discord.js-selfbot');
+const fs = require('fs');
+const client = new Client();
+const config = require('./config.json');
+
+const prefix = config.prefix;
+const token = config.token;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+
+client.on('ready', () => {
+    client.user.setActivity(`${prefix}help`)
+    console.log(`${client.user.username} ✅`)
+})
+
+client.on('message', async message =>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if(cmd.length == 0 ) return;
+
+    let command = client.commands.get(cmd);
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if(command) command.run(message, args, command, client);
+})
+
+client.login(token)
